a487e4d85f5bf802175ffef9887c90cc95ef9ca2
samik shk <samik.shakya8848@gmail.com>
2022-11-03 18:09:27 +0545

    testimonial

--- js/script.js
+++ js/script.js
@@ -155,6 +155,25 @@ $(function() {
     
 });
 
+//<====================  Testimonial  =====================>
+
+
+$(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: true,
+        // navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+
+        navText: false
+    });
+});
+
+
 
 
 
